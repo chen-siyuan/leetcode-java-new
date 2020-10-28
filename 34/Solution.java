81199e518c616bcd2ada548c4898118878ce09ae
Siyuan Chen <daniel@apextone.com>
2020-10-28 19:57:04 -0400

    Added 34
    Note

--- 34/Solution.java
+++ 34/Solution.java
@@ -3,15 +3,30 @@ public class Solution {
     public int[] searchRange(int[] nums, int target) {
 
         int n = nums.length;
-        if(n == )
+        int[] res = new int[]{-1, -1};
 
-        int head = 
-    
-    
-    
-    
-    
-    
+        int l = 0;
+        int r = n;
+
+        while(l < r) {
+            int m = (l + r) / 2;
+            if(nums[m] < target) l = m + 1;
+            else r = m;
+        }
+
+        if(l == n || nums[l] != target) return res;
+        res[0] = l;
+
+        r = n - 1;
+
+        while(l < r) {
+            int m = (l + r + 1) / 2;
+            if(nums[m] <= target) l = m;
+            else r = m - 1;
+        }
+        res[1] = r;
+
+        return res;
     }
 
 }
