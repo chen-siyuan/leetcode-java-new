c49778306737c5cd143afa520813a811efc3be75
Siyuan Chen <daniel@apextone.com>
2020-10-28 19:27:40 -0400

    Added 33, 34
    Note

---
+++ 34/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+
+    public int[] searchRange(int[] nums, int target) {
+
+        int n = nums.length;
+        if(n == )
+
+        int head = 
+    
+    
+    
+    
+    
+    
+    }
+
+}
