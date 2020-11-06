147be61044cd3ac837175053960f54aec415c4a7
Siyuan Chen <daniel@apextone.com>
2020-11-06 18:21:22 -0500

    Updated 42
    Note

--- 42/Solution.java
+++ 42/Solution.java
@@ -3,20 +3,20 @@ public class Solution {
     public int trap(int[] height) {
 
         int n = height.length;
-        if(n == 0) return 0;
-
-        int[] rs = new int[n];
-
-        rs[n - 1] = 0;
-        for(int i=n-2; i >= 0; i--) rs[i] = Math.max(rs[i + 1], height[i + 1]);
-
-        int l = 0;
         int res = 0;
-        for(int i=1; i < n - 1; i++) {
-            l = Math.max(l, height[i - 1]);
-            if(height[i] < Math.min(l, rs[i])) res += Math.min(l, rs[i]) - height[i];
+        int l = 0;
+        int r = n - 1;
+        int lm = 0;
+        int rm = 0;
+
+        while(l <= r) if(lm < rm) {
+            if(height[l] >= lm) lm = height[l++];
+            else res += lm - height[l++];
+        } else {
+            if(height[r] >= rm) rm = height[r--];
+            else res += rm - height[r--];
         }
-    
+
         return res;
     }
 
