d361173d694d3212ca25b520efd297c5e285609c
Siyuan Chen <daniel@apextone.com>
2020-11-21 21:28:35 -0500

    Added 79
    Note

--- 79/Solution.java
+++ 79/Solution.java
@@ -5,18 +5,23 @@ public class Solution {
     };
 
     public boolean exist(char[][] board, String word) {
-        for(int i=0; i < board.length; i++) for(int j=0; j < board[0].length; j++) if(DFS(board, new boolean[board.length][board[0].length], i, j, word)) return true;
+
+        boolean[][] memo = new boolean[board.length][board[0].length];
+
+        for(int i=0; i < board.length; i++) for(int j=0; j < board[0].length; j++)
+            if(DFS(board, memo, i, j, word, 0)) return true;
+
         return false;
     }
 
-    public boolean DFS(char[][] board, boolean[][] searched, int x, int y, String remaining) {
+    public boolean DFS(char[][] board, boolean[][] memo, int x, int y, String word, int index) {
 
-        if(remaining.length() == 0) return true;
-        if(x < 0 || x >= board.length || y < 0 || y >= board[0].length || searched[x][y] || board[x][y] != remaining.charAt(0)) return false;
+        if(index == word.length()) return true;
+        if(x < 0 || x >= board.length || y < 0 || y >= board[0].length || memo[x][y] || board[x][y] != word.charAt(index)) return false;
 
-        searched[x][y] = true;
-        for(int[] dir: DIRECTIONS) if(DFS(board, searched, x + dir[0], y + dir[1], remaining.substring(1, remaining.length()))) return true;
-        searched[x][y] = false;
+        memo[x][y] = true;
+        for(int[] dir: DIRECTIONS) if(DFS(board, searched, x + dir[0], y + dir[1], word, index + 1)) return true;
+        memo[x][y] = false;
 
         return false;
     }
